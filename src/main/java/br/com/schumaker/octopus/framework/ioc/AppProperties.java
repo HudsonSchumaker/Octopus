249c5c4926ab86553df3161c1d592b0773060072
HudsonSchumaker <hudson.schumaker@me.com>
2024-08-17 17:45:24 +0200

    Oc framework: changes for env. contexts.

--- src/main/java/br/com/schumaker/octopus/framework/ioc/AppProperties.java
+++ src/main/java/br/com/schumaker/octopus/framework/ioc/AppProperties.java
@@ -2,16 +2,18 @@ package br.com.schumaker.octopus.framework.ioc;
 
 public class AppProperties {
 
-    protected static final String APP_NAME = "Octopus";
-    protected static final String APP_VERSION = "0.0.0";
+
+    public static final String APP_NAME = "Octopus";
+    public static final String APP_VERSION = "0.10.0";
+    public static final String DB_DRIVER = "db.driver";
+    public static final String DB_URL = "db.url";
+    public static final String DB_USER = "db.user";
+    public static final String DB_PASSWORD = "db.password";
+    public static final String APPLICATION_PROPERTIES_FILE_NAME = "application.properties";
+    public static final String DEFAULT_VALUE_NAME = "oc.annotation.default.value";
+
     protected static final String SERVER_PORT = "server.port";
     protected static final String SERVER_CONTEXT = "server.context";
-    protected static final String DB_DRIVER = "db.driver";
-    protected static final String DB_URL = "db.url";
-    protected static final String DB_USER = "db.user";
-    protected static final String DB_PASSWORD = "db.password";
-
-    public static final String DEFAULT_VALUE_NAME = "oc.annotation.default.value";
 
     private AppProperties() {}
 }
