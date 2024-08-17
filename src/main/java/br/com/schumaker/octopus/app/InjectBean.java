ebb30cb8a9f57ec4a4af99752d0bdc582ebf64af
HudsonSchumaker <hudson.schumaker@me.com>
2024-08-17 21:14:35 +0200

    Oc framework: injection handler.

--- src/main/java/br/com/schumaker/octopus/app/InjectBean.java
+++ src/main/java/br/com/schumaker/octopus/app/InjectBean.java
@@ -2,10 +2,16 @@ package br.com.schumaker.octopus.app;
 
 import br.com.schumaker.octopus.framework.annotations.Component;
 import br.com.schumaker.octopus.framework.annotations.Inject;
+import br.com.schumaker.octopus.framework.web.http.HttpRestTemplate;
 
 @Component
 public class InjectBean {
 
     @Inject
     private ProductDTO productDTO;
+
+    @Inject
+    public InjectBean(HttpRestTemplate httpRestTemplate, ProductDTO productDTO) {
+        System.out.println("InjectBean constructor");
+    }
 }
