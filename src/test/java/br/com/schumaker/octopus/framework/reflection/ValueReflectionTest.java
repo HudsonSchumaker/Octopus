e25f70918628cf014abcd5cc0cb03fd2b0bd9ad2
HudsonSchumaker <hudson.schumaker@me.com>
2024-08-18 00:43:34 +0200

    Oc framework: add ValueReflection test.

--- src/test/java/br/com/schumaker/octopus/framework/reflection/ValueReflectionTest.java
+++ src/test/java/br/com/schumaker/octopus/framework/reflection/ValueReflectionTest.java
@@ -2,10 +2,14 @@ package br.com.schumaker.octopus.framework.reflection;
 
 import br.com.schumaker.octopus.framework.annotations.Value;
 import br.com.schumaker.octopus.framework.ioc.Environment;
+import br.com.schumaker.octopus.framework.exception.OctopusException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ValueReflectionTest {
     private ValueReflection valueReflection;
@@ -25,29 +29,113 @@ public class ValueReflectionTest {
 
         @Value("test.int")
         private int testInt;
+
+        @Value("test.unsupported")
+        private Object unsupportedField;
+
+        private String noAnnotationField;
+
+        public void testMethod(@Value("test.string") String param) {
+            // Method for testing parameter injection
+        }
+
+        public void unsupportedTypeMethod(@Value("test.unsupported") Object param) {
+            // Method for testing unsupported type
+        }
+
+        public void wrongType(@Value("test.invalid.type") int param) {
+            // Method for testing wrong type
+        }
+
+        public void noAnnotationMethod(String param) {
+            // Method for testing no annotation
+        }
     }
 
     @Test
-    public void testInjectFieldValue() {
+    public void testInjectFieldValueWithSupportedTypes() {
         // Arrange
         TestClass testInstance = new TestClass();
+
+        // Act
         valueReflection.injectFieldValue(testInstance);
 
-        // Act & Assert
+        // Assert
         assertEquals("testValue", testInstance.testString);
         assertEquals(123, testInstance.testInt);
     }
 
     @Test
-    public void testInjectParameterValue() {
+    public void testInjectFieldValueWithUnsupportedType() {
         // Arrange
         TestClass testInstance = new TestClass();
-        environment.setKey("test.string", "testValue");
 
         // Act
-        Object instance = valueReflection.injectFieldValue(testInstance);
+        valueReflection.injectFieldValue(testInstance);
 
         // Assert
-        assertEquals("testValue", ((TestClass)instance).testString);
+        assertNull(testInstance.unsupportedField);
+    }
+
+    @Test
+    public void testInjectFieldValueWithoutAnnotation() {
+        // Arrange
+        TestClass testInstance = new TestClass();
+
+        // Act
+        valueReflection.injectFieldValue(testInstance);
+
+        // Assert
+        assertNull(testInstance.noAnnotationField);
+    }
+
+    @Test
+    public void testInjectParameterValueWithSupportedType() throws NoSuchMethodException {
+        // Arrange
+        Method method = TestClass.class.getMethod("testMethod", String.class);
+        Parameter parameter = method.getParameters()[0];
+
+        // Act
+        Object injectedValue = valueReflection.injectParameterValue(parameter);
+
+        // Assert
+        assertEquals("testValue", injectedValue);
+    }
+
+    @Test
+    public void testInjectParameterValueWithUnsupportedType() throws NoSuchMethodException {
+        // Arrange
+        Method method = TestClass.class.getMethod("unsupportedTypeMethod", Object.class);
+        Parameter parameter = method.getParameters()[0];
+
+        // Act
+        Object injectedValue = valueReflection.injectParameterValue(parameter);
+
+        // Assert
+        assertNull(injectedValue);
+    }
+
+    @Test
+    public void testInjectParameterValueWithoutAnnotation() throws NoSuchMethodException {
+        // Arrange
+        Method method = TestClass.class.getMethod("noAnnotationMethod", String.class);
+        Parameter parameter = method.getParameters()[0];
+
+        // Act
+        Object injectedValue = valueReflection.injectParameterValue(parameter);
+
+        // Assert
+        assertNull(injectedValue);
+    }
+
+    @Test
+    public void testInjectParameterValueThrowsException() throws NoSuchMethodException {
+        // Arrange
+        Method method = TestClass.class.getMethod("wrongType", int.class);
+        Parameter parameter = method.getParameters()[0];
+        environment.setKey("test.invalid.type", "invalid-Int");
+
+        // Act & Assert
+        assertThrows(OctopusException.class, () -> valueReflection.injectParameterValue(parameter));
     }
 }
