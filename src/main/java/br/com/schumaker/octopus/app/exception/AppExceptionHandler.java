d51d6f8cbf99c8ae281eeb362c00cc11e3dedb76
HudsonSchumaker <hudson.schumaker@me.com>
2024-08-31 18:36:47 +0200

    Oc app: change for fluent api.

--- src/main/java/br/com/schumaker/octopus/app/exception/AppExceptionHandler.java
+++ src/main/java/br/com/schumaker/octopus/app/exception/AppExceptionHandler.java
@@ -3,6 +3,7 @@ package br.com.schumaker.octopus.app.exception;
 import br.com.schumaker.octopus.framework.annotations.exception.ExceptionHandler;
 import br.com.schumaker.octopus.framework.annotations.exception.GlobalExceptionHandler;
 import br.com.schumaker.octopus.framework.exception.ErrorView;
+import br.com.schumaker.octopus.framework.web.http.Http;
 import br.com.schumaker.octopus.framework.web.view.ResponseView;
 
 import java.io.IOException;
@@ -12,11 +13,11 @@ public class AppExceptionHandler {
 
     @ExceptionHandler
     public ResponseView<String> handle(Exception e) {
-       return ResponseView.of("Exception", 300);
+        return ResponseView.conflict().body(e.getMessage()).build();
     }
 
     @ExceptionHandler(IOException.class)
     public ResponseView<ErrorView> handle2(IOException e) {
-        return ResponseView.of(new ErrorView(e.getMessage(), 400), 400);
+        return ResponseView.badRequest().body(new ErrorView(e.getMessage(), "#8x0008")).build();
     }
 }
