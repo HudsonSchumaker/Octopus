5a33311f242c9b6ba93fae3f883eeaf3941d43ee
HudsonSchumaker <hudson.schumaker@me.com>
2024-09-07 12:25:56 +0200

    Oc app: rename exception method handlers.

--- src/main/java/br/com/schumaker/octopus/app/exception/AppExceptionHandler.java
+++ src/main/java/br/com/schumaker/octopus/app/exception/AppExceptionHandler.java
@@ -3,7 +3,6 @@ package br.com.schumaker.octopus.app.exception;
 import br.com.schumaker.octopus.framework.annotations.exception.ExceptionHandler;
 import br.com.schumaker.octopus.framework.annotations.exception.GlobalExceptionHandler;
 import br.com.schumaker.octopus.framework.exception.ErrorView;
-import br.com.schumaker.octopus.framework.web.http.Http;
 import br.com.schumaker.octopus.framework.web.view.ResponseView;
 
 import java.io.IOException;
@@ -12,12 +11,12 @@ import java.io.IOException;
 public class AppExceptionHandler {
 
     @ExceptionHandler
-    public ResponseView<String> handle(Exception e) {
+    public ResponseView<String> methodHandlerException(Exception e) {
         return ResponseView.conflict().body(e.getMessage()).build();
     }
 
     @ExceptionHandler(IOException.class)
-    public ResponseView<ErrorView> handle2(IOException e) {
+    public ResponseView<ErrorView> methodHandlerIOException(IOException e) {
         return ResponseView.badRequest().body(new ErrorView(e.getMessage(), "#8x0008")).build();
     }
 }
