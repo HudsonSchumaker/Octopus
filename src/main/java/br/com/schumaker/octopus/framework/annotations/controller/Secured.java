19f35f4468b3ec96abe30b044bfa0cbc21dde4c7
HudsonSchumaker <hudson.schumaker@me.com>
2024-09-07 12:51:39 +0200

    Oc framework: add security annotation.

---
+++ src/main/java/br/com/schumaker/octopus/framework/annotations/controller/Secured.java
@@ -0,0 +1,40 @@
+package br.com.schumaker.octopus.framework.annotations.controller;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * The @Secured annotation is used to mark a method as requiring authentication.
+ * This annotation can be applied to methods to indicate that the method should be
+ * secured and require authentication.
+ *
+ * <p>
+ * Example usage:
+ * </p>
+ *
+ * <pre>
+ * {@code
+ * @Controller("/api")
+ * public class MyController {
+ *
+ *     @Secured
+ *     @Get(value = "/search")
+ *     public ResponseView<ProductView> searchProduct(@QueryParam("q") String query) {
+ *         // Handle GET request
+ *         return ResponseView.of(new ProductView(), Http.HTTP_200);
+ *     }
+ * }
+ * }
+ * </pre>
+ *
+ * @see Get
+ * @see Controller
+ *
+ * @author Hudson Schumaker
+ * @version 1.0.0
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Secured {}
