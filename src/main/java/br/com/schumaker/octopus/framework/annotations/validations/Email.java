b381f724254ceba429f2a8a54f8b0dba3c8478e7
HudsonSchumaker <hudson.schumaker@me.com>
2024-09-14 23:06:03 +0200

    Oc project: update email validation.

--- src/main/java/br/com/schumaker/octopus/framework/annotations/validations/Email.java
+++ src/main/java/br/com/schumaker/octopus/framework/annotations/validations/Email.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import static br.com.schumaker.octopus.framework.reflection.validation.ValidationReflection.EMAIL_VALIDATION_MESSAGE;
+import static br.com.schumaker.octopus.framework.reflection.validation.EmailValidation.EMAIL_VALIDATION_MESSAGE;
 
 /**
  * The @Email annotation is used to mark a field as an email address.
@@ -35,6 +35,7 @@ import static br.com.schumaker.octopus.framework.reflection.validation.Validatio
  * @see Past
  * @see Range
  * @see Email
+ * @see Future
  * @see NotNull
  * @see NotBlank
  * @see NotEmpty
