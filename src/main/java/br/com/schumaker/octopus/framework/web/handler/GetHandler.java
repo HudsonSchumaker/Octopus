90115699db70cc85fc81cc53c2d6b62f232af6a4
HudsonSchumaker <hudson.schumaker@me.com>
2024-09-15 12:59:14 +0200

    Oc project: refactored HTTP request logic.

--- src/main/java/br/com/schumaker/octopus/framework/web/handler/GetHandler.java
+++ src/main/java/br/com/schumaker/octopus/framework/web/handler/GetHandler.java
@@ -1,20 +1,20 @@
 package br.com.schumaker.octopus.framework.web.handler;
 
+import br.com.schumaker.octopus.framework.annotations.controller.Get;
 import br.com.schumaker.octopus.framework.annotations.controller.PathVariable;
 import br.com.schumaker.octopus.framework.exception.OctopusException;
 import br.com.schumaker.octopus.framework.ioc.IoCContainer;
 import br.com.schumaker.octopus.framework.model.TypeConverter;
 import br.com.schumaker.octopus.framework.web.http.Http;
 import br.com.schumaker.octopus.framework.web.http.HttpRequest;
+import br.com.schumaker.octopus.framework.web.http.HttpRequestHeader;
 import br.com.schumaker.octopus.framework.web.http.HttpResponse;
 
-import  java.util.Arrays;
-
-import static br.com.schumaker.octopus.framework.web.http.Http.GET;
+import static br.com.schumaker.octopus.framework.web.http.Http.HTTP_GET;
 
 /**
  * The GetHandler class.
- * This class is responsible for processing GET requests.
+ * This class is responsible for processing HTTP_GET requests.
  *
  * @author Hudson Schumaker
  * @version 1.0.0
@@ -22,51 +22,52 @@ import static br.com.schumaker.octopus.framework.web.http.Http.GET;
 public final class GetHandler implements RequestHandler {
     private final IoCContainer container = IoCContainer.getInstance();
 
+    /**
+     * Process the HTTP_GET request.
+     *
+     * @param request the HTTP request.
+     * @return the HTTP response.
+     */
     @Override
     public HttpResponse processRequest(HttpRequest request) {
-        String fullUrl = request.fullUrl();
         int httpCode = Http.HTTP_200;
         String applicationType = "";
         String response = "";
 
-        var requestHeaders = request.exchange().getRequestHeaders();
-
-        // TODO: nightmare code, refactor this
-        String[] split = fullUrl.split("/");
-        String controllerRoute = split.length > 4 ? "/" + split[4] : "/";
-        String methodPath = split.length > 5 ? "/" + String.join("/", Arrays.copyOfRange(split, 5, split.length)) : "/";
+        var routeAndMethodPath = request.getControllerRouteAndMethodPath();
+        String controllerRoute = routeAndMethodPath.first();
+        String methodPath = routeAndMethodPath.second();
 
         var controller = container.getController(controllerRoute);
         if (controller != null) {
-            var mappingAndMethodAndParams = controller.getMethod(methodPath, GET);
+            var mappingAndMethodAndParams = controller.getMethod(methodPath, HTTP_GET);
 
-            if (mappingAndMethodAndParams != null) {
-                var mapping = mappingAndMethodAndParams.first();
-                var method = mappingAndMethodAndParams.second();
-                var methodReturnType = method.getReturnType();
-                var parameters = mappingAndMethodAndParams.third();
-                var args = new Object[parameters.size()];
-                var pathVariables = controller.extractPathVariables(mapping, methodPath);
+            var mapping = mappingAndMethodAndParams.first();
+            var method = mappingAndMethodAndParams.second();
+            var methodReturnType = method.getReturnType();
+            var parameters = mappingAndMethodAndParams.third();
+            var arguments = new Object[parameters.size()];
+            var pathVariables = controller.extractPathVariables(mapping, methodPath);
 
-                for (short i = 0; i < parameters.size(); i++) {
-                    if (parameters.get(i).isAnnotationPresent(PathVariable.class)) {
-                        args[i] = this.convertToType(pathVariables.get(i), parameters.get(i).getType());
-                    }
+            for (short i = 0; i < parameters.size(); i++) {
+                if (parameters.get(i).isAnnotationPresent(PathVariable.class)) {
+                    arguments[i] = this.convertToType(pathVariables.get(i), parameters.get(i).getType());
+                    continue;
                 }
 
-                // TODO: try to resolve this
-                httpCode = method.getAnnotation(br.com.schumaker.octopus.framework.annotations.controller.Get.class).httpCode();
-                applicationType = method.getAnnotation(br.com.schumaker.octopus.framework.annotations.controller.Get.class).type();
-
-                try {
-                    Object result = method.invoke(controller.getInstance(), args);
-                    return new HttpResponse(methodReturnType, result, httpCode, applicationType, request.exchange());
-                } catch (Exception e) {
-                    throw new OctopusException("Error invoking method", e);
+                if (parameters.get(i).getType().equals(HttpRequestHeader.class)) {
+                    arguments[i] = new HttpRequestHeader(request.getRequestHeaders());
                 }
-            } else {
-                httpCode = Http.HTTP_404;
-                response = "Method not found!";
+            }
+
+            httpCode = method.getAnnotation(Get.class).httpCode();
+            applicationType = method.getAnnotation(Get.class).type();
+
+            try {
+                Object result = method.invoke(controller.getInstance(), arguments);
+                return new HttpResponse(methodReturnType, result, httpCode, applicationType, request.exchange());
+            } catch (Exception e) {
+                throw new OctopusException("Error invoking method", e);
             }
         } else {
             httpCode = Http.HTTP_404;
--- src/main/java/br/com/schumaker/octopus/framework/web/http/HttpRequest.java
+++ src/main/java/br/com/schumaker/octopus/framework/web/http/HttpRequest.java
@@ -1,6 +1,15 @@
 package br.com.schumaker.octopus.framework.web.http;
 
+import br.com.schumaker.octopus.framework.model.Pair;
 import com.sun.net.httpserver.HttpExchange;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This class represents a Http request.
@@ -12,7 +21,48 @@ import com.sun.net.httpserver.HttpExchange;
  * @since 1.0.0
  */
 public record HttpRequest (String fullUrl, HttpExchange exchange) {
-    public String getVerb() {
-        return exchange.getRequestMethod();
+
+    /**
+     * Get the controller route and method path.
+     *
+     * @return A Pair with the controller route and method path.
+     */
+    public Pair<String, String> getControllerRouteAndMethodPath() {
+        String[] split = fullUrl.split("/");
+        String controllerRoute = split.length > 4 ? "/" + split[4] : "/";
+        String methodPath = split.length > 5 ? "/" + String.join("/", Arrays.copyOfRange(split, 5, split.length)) : "/";
+
+        return new Pair<>(controllerRoute, methodPath);
+    }
+
+    /**
+     * Get the request headers.
+     *
+     * @return `Map` with the request headers.
+     */
+    public Map<String, String> getRequestHeaders() {
+        Map<String, String> headers = new HashMap<>();
+        exchange.getRequestHeaders().forEach((k, v) -> headers.put(k, String.join(", ", v)));
+        return headers;
+    }
+
+    /**
+     * Reads the request body from the HttpExchange object.
+     *
+     * @return the request body as a string
+     * @throws Exception if an error occurs during request body reading
+     */
+    public String readRequestBody() throws Exception {
+        StringBuilder requestBody = new StringBuilder();
+        try (InputStream is = exchange.getRequestBody();
+             InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
+             BufferedReader br = new BufferedReader(isr)) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                requestBody.append(line);
+            }
+        }
+        return requestBody.toString();
     }
 }
