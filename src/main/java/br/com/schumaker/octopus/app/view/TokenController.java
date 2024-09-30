7ed1da52dbf026ad4dc924384a2179072fe91937
schumaker <hudson.schumaker@me.com>
2024-09-30 21:05:09 +0200

    Oc-framework: add documentation.

--- src/main/java/br/com/schumaker/octopus/app/view/TokenController.java
+++ src/main/java/br/com/schumaker/octopus/app/view/TokenController.java
@@ -6,6 +6,16 @@ import br.com.schumaker.octopus.framework.annotations.controller.Post;
 import br.com.schumaker.octopus.framework.security.JwtManager;
 import br.com.schumaker.octopus.framework.web.view.ResponseView;
 
+/**
+ * The TokenController class.
+ * It is responsible for controlling the token operations.
+ *
+ * @see PathVariable
+ * @see JwtManager
+ *
+ * @author Hudson Schumaker
+ * @version 1.0.0
+ */
 @Controller("/token")
 public class TokenController {
 
